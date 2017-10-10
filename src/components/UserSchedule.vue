9a3d1db6b45e756d1020220651f722904be74906
Baal <baal482968@gmail.com>
2017-10-10 21:01:14 +0800

    Added Calendar and Header image

--- src/components/UserSchedule.vue
+++ src/components/UserSchedule.vue
@@ -1,9 +1,24 @@
 <template lang="html">
-
+  <vue-event-calendar :events="demoEvents"></vue-event-calendar>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      demoEvents: [
+        {
+          date: '2017/10/10',
+          title: 'Finance Meeting',
+          desc: 'Remember to bring laptop'
+        },
+        {
+          date: '2017/11/12',
+          title: 'Bar'
+        }
+      ]
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import App from './App.vue';
 import { store } from './store/store';
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
+// Calendar
+import 'vue-event-calendar/dist/style.css' //^1.1.10, CSS has been extracted as one file, so you can easily update it.
+import vueEventCalendar from 'vue-event-calendar'
 
 
 Vue.use(BootstrapVue);
+
+Vue.use(vueEventCalendar, {locale: 'en'}) //locale can be 'zh' , 'en' , 'es', 'pt-br', 'ja', 'ko', 'fr', 'it', 'ru', 'de'
 
 new Vue({
   el: '#app',
