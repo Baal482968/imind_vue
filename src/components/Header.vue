d8946f263840b246037bf6149c2982bcce2e1ffb
niconame <hazelluf@gmail.com>
2017-10-11 15:47:13 +0800

    change logo's URL

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       <b-img center
-      src="https://lh3.googleusercontent.com/pRLaS8DP0vSo0ZeMfHaw2ooKe9j9Pi9MuDZ_9oMhDodnUe23_1ciiCLqSTD3KJAmChfFh2M6sQGqD_DKm5qY6JCjEmzpVh0ySIF62ncpIyH61-GI8bjmITKWAOZt82LincUP5S_RRbY-2kXnvuDJWTEFHAzTnIENSe2FNS7i9c6qychmKWPllaZW9ZLhpKhbw2eYrajG0bkp89dG7JLLnD4KRs3Z2YexttHk4lMjYjQO-9pte6a3oa-ZSrqozK2uHvpVk5Dd30hlLxHARJksEukBYjK7l2WVBVl7RP_8okdAlbVpqwoJJVml2G4pIPebZXuhHKtfUhvL2NowDPFIUOkfPVbwiOZdNJLXMQKXvtb4HMzLb1MTdx2PMnSmuy_bzeNfcvVaahgoPTLOuTgBjPAviT98lPXKBXYyc8QG1kpNLDbb0bNl-O4BmG8pXpzHEpTIbaXI2Oq7mb-4_5PLk3YYJFHT2_S8A4-YGT7UvikXWIR0DleABCFdKNRekq6-Ucs8bZq6kYAjgFrzf7dx76fQDaO7iK5zuVS9Fb0UOwFG3mH5AHIENGnaRdQBc0UUrnRDtjhIN1z7GF1WXzl1DqdxvEAObC-CZwNXuKBN9A=w696-h464-no"
+      src="https://i.imgur.com/A2ukCGP.png"
       fluid alt="Responsive image"
       id="pic"
       />
