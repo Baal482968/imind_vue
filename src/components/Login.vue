3e36f5cb07a579f74cdd1b98ebd016b15cd92f62
Baal Wu <baal482968@gmail.com>
2017-10-13 11:03:14 +0800

    fix login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,26 @@
 <template lang="html">
   <div id="login">
     <div class="jumbotron vertical-center">
-      <b-container fluid class="mx-auto w-25">
-        <b-row class="my-1">
-          <b-col sm="10">
+      <b-container fluid class="w-25">
+        <h1>Please Login</h1>
+        <b-row class="my-5">
+          <b-col sm="3">
             <label for="input-large" class="my-3">Username:</label>
+          </b-col>
+          <b-col sm="9">
             <b-form-input id="input-large" size="lg" type="text" placeholder="Enter your name"></b-form-input>
+          </b-col>
+          <b-col sm="3">
             <label for="input-large" class="my-3">Password:</label>
+          </b-col>
+          <b-col sm="9">
             <b-form-input id="input-large" size="lg" type="password" placeholder="Enter your password"></b-form-input>
           </b-col>
-          <b-col sm="2" class="btn">
-            <b-button id="submitbtn" class="my-auto">Login</b-button>
-          </b-col>
+        </b-row>
+        <b-row class="my-2 justify-content-center align-items-center">
+          <div class="btn">
+            <b-button>Login</b-button>
+          </div>
         </b-row>
       </b-container>
     </div>
@@ -24,17 +33,17 @@ export default {
 </script>
 
 <style lang="css" scoped>
-
+h1{
+  text-align: center;
+}
 .vertical-center {
-  min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
-  min-height: 100vh; /* These two lines are counted as one :-)       */
+  min-height: 60%;  /* Fallback for browsers do NOT support vh unit */
+  min-height: 60vh; /* These two lines are counted as one :-)       */
 
   display: flex;
   align-items: center;
 }
-.btn{
-  display: flex;
-  align-items: center;
-  min-height: 40%;
+.my-3{
+  font-size: 130%;
 }
 </style>
