bbef8e8012a9950014a527e1892eea67743a59f5
BaalWu <baal482968@gmail.com>
2017-12-21 10:06:26 +0800

    create note part

--- src/components/note/Editor.vue
+++ src/components/note/Editor.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="note-editor">
-    <textarea
-      :value="activeNoteText"
-      @input="editNote"
-      class="form-control">
-    </textarea>
+    
   </div>
 </template>
 
 <script>
 
 export default {
-  vuex: {
-    getters: {
-      activeNoteText: state => state.activeNote.text
-    },
-    actions: {
-      editNote
-    }
-  }
+  
 }
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -47,31 +47,6 @@ export const store = new Vuex.Store({
       // }
     },
     mutations: {
-      ADD_NOTE(state) {
-        const newNote = {
-          text: 'New note',
-          favorite: false
-        }
-        state.notes.push(newNote)
-        state.activeNote = newNote
-      },
-
-      EDIT_NOTE(state, text) {
-        state.activeNote.text = text
-      },
-
-      DELETE_NOTE(state) {
-        state.notes.$remove(state.activeNote)
-        state.activeNote = state.notes[0]
-      },
-
-      TOGGLE_FAVORITE(state) {
-        state.activeNote.favorite = !state.activeNote.favorite
-      },
-
-      SET_ACTIVE_NOTE(state, note) {
-        state.activeNote = note
-      }
       // addNode: state => {
       //   state.projectNode.push({
       //     x:100, y:100, r:40, stroke:'green', strokeWidth:4, fill:'yellow'
